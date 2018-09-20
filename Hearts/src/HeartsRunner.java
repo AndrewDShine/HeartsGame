c6f907de9b9b494b30fa838054f607d0ea4ae392
andrew.shine <andrew.shine@MU-C1-10.mullen.pvt.k12.co.us>
2018-09-20 14:10:23 -0600

    working on fixing index errors

--- Hearts/src/HeartsRunner.java
+++ Hearts/src/HeartsRunner.java
@@ -4,16 +4,54 @@ import java.util.Scanner;
 public class HeartsRunner
 	{
 		static ArrayList<Card> deck = new ArrayList<Card>();
+		static ArrayList<Card> pool = new ArrayList<Card>();
+		static ArrayList<Card> discardPile = new ArrayList<Card>();
+		static ArrayList<HumanPlayer> players = new ArrayList<HumanPlayer>();
+		static Scanner userStringPut = new Scanner(System.in);
 		
 		public static void main(String[] args)
 			{
-				
+				generateDeck();
+				System.out.println("Ready to play Hearts? It's a four-player game, so you'd better grab some friends!");
+				makePlayers(4);
 			}
 		public static void generateDeck()
 		{
 			for(int i = 1; i <= 13; i++)
 				{
 					deck.add(new Card("Hearts", i));
+				}
+			for(int i = 1; i <=13; i++)
+				{
+					deck.add(new Card("Spades", i));
+				}
+			for(int i = 1; i <=13; i++)
+				{
+					deck.add(new Card("Clubs", i));
+				}
+			for(int i = 1; i <=13; i++)
+				{
+					deck.add(new Card("Diamonds", i));
+				}
+		}
+		public static void makePlayers(int numOfPlayers)
+		{
+			for (int i = 1; i <= numOfPlayers; i++)
+				{
+					System.out.println("Hi, Player "+i+"! What's your name?");
+					players.add(new HumanPlayer(userStringPut.nextLine()));
+					System.out.println("Nice to meet ya, "+players.get((i-1)).getName()+"!");
+					System.out.print("\n");
+				}
+			
+			for(int i = 0; i < numOfPlayers; i++)
+				{
+					for(int j = 0; i < 13; j++)
+						{
+							players.get(i).addToHand(deck.get(j));
+							System.out.println(deck.get(j).getRank());
+							System.out.println("iteration");
+						}
 				}
 		}
 
---
+++ Hearts/src/HumanPlayer.java
@@ -0,0 +1,35 @@
+import java.util.ArrayList;
+
+public class HumanPlayer
+	{
+		private static ArrayList<Card> hand = new ArrayList<Card>();
+		private String name;
+		
+		public HumanPlayer(String n)
+		{
+			name = n;
+
+		}
+
+		public static ArrayList<Card> getHand()
+			{
+				return hand;
+			}
+		public static Card getCard(int i)
+		{
+			return hand.get(i);
+		}
+		public String getName()
+			{
+				return name;
+			}
+		public static void addToHand(Card c)
+		{
+			hand.add(c);
+		}
+		public static int returnSizeOfHand()
+		{
+			return hand.size();
+		}
+		
+	}
